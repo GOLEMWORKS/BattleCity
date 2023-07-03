995ef27ad82d8c95a72c565531e4423e354a018f
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-07-03 19:14:39 +0200

    Отрисовка цветов на объекте fix

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,16 @@ GLfloat colors[] =
 };
 
 
-//Вертексный шейдер
 const char* vertex_shader =
 "#version 460\n"
 "layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color"
-"out vec3 color; "
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
 "void main() {"
 "   color = vertex_color;"
 "   gl_Position = vec4(vertex_position, 1.0);"
 "}";
 
-//Фрагментный шейдер
 const char* fragment_shader =
 "#version 460\n"
 "in vec3 color;"
